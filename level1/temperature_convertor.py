c3c45c76ff0f4434efec57283a3342894041e89f
Silver9876 <106231902+Silver9876@users.noreply.github.com>
2023-11-19 21:39:39 +0530

    Update temperature_convertor.py

--- level1/temperature_convertor.py
+++ level1/temperature_convertor.py
@@ -1,22 +1,22 @@
 def fahrenhit():
-    value=int(input("enter the temparature: "))
-    temp=(value * (9/5)) + 32
-    print(f"the temperature in fahrenhit is: {temp}")
-    
-def celceus():
-    value=int(input("enter the temparature: "))
-    temp=(value-32)*(5/9)
-    print("the temperature in celceus is: ")
-    
-def default():
-    print("you didn't choose the option")
+    value = int(input("Enter the temperature: "))
+    temp = (value * (9/5)) + 32
+    print(f"The temperature in Fahrenheit is: {temp}")
 
-print("1.celcus to fahrenhit\n2.fahrenhit to celceus\n")
-choice=int(input("enter your option: "))
+def celceus():
+    value = int(input("Enter the temperature: "))
+    temp = (value - 32) * (5/9)
+    print(f"The temperature in Celsius is: {temp}")
+
+def default():
+    print("You didn't choose a valid option.")
+
+print("1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\n")
+choice = int(input("Enter your option: "))
 cases = {
-        1: fahrenhit,
-        2: celceus
-    }
+    1: fahrenhit,
+    2: celceus
+}
 
 selected_case = cases.get(choice, default)
 selected_case()
